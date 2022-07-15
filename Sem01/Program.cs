55421568243512be11365dbab307f7c989a53631
Kliiim89 <mark1989mail@mail.ru>
2022-07-15 16:24:02 +0300

    Задача 34

--- Sem01/Program.cs
+++ Sem01/Program.cs
@@ -1,38 +1,32 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными 
-// трёхзначными числами. Напишите программу, которая покажет 
-// количество чётных чисел в массиве.
+// трёхзначными числами. Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
 
 int[] numbers = new int[10];
 
-void FillArray(int[] array, int min, int max)
-{
-  for (int i = 0; i<array.Length; i++ )
-  {
+void FillArray(int[] array, int min, int max){
+  for (int i = 0; i<array.Length; i++ ){
     array[i] = new Random().Next(min, max);
   }
 }
 
-void WriteArray(int[] array)
-{
-    for (int i = 0; i<array.Length; i++ )
-    {
+void WriteArray(int[] array){
+    for (int i = 0; i<array.Length; i++ ){
     Console.Write(array[i] + " ");
-    }
+  }
   Console.WriteLine();
 }
 
-int QuantityPositive(int[] array)
-{
+int QuantityPositive(int[] array){
     int quantity = 0;
-    for (int i = 0; i<array.Length; i++ )
-    {
+    for (int i = 0; i<array.Length; i++ ){
     if (array[i] % 2 == 1)
     {
       quantity++;
     }
-    }
+  }
   return quantity;
 }
 
